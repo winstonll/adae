d7df76b7612d1b6ccc7ab09497e64c05ef8b0f75
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-11-17 23:10:04 -0500

    generated users controller for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  #api
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+      resources :microposts, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
