a6327dd7942df0a392232bad9cfbd62f22c3bbe5
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-11-23 18:21:32 -0500

    editing the correct production.rb file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,11 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+server '159.203.22.61', user: 'root', roles: %w{web app}
+
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
-
 
 # role-based syntax
 # ==================
@@ -16,6 +16,10 @@
 # group is considered to be the first unless any  hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
+
+role :app, %w{deploy@159.203.22.61}
+role :web, %w{deploy@159.203.22.61}
+role :db,  %w{deploy@159.203.22.61}
 
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
