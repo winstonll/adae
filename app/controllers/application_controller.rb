d5ed2586a30bac0d93755fc7f3d7c2b41850e643
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-11-25 15:13:08 -0500

    changed landing css to erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   #   redirect_to root_url, :alert => exception.message
   # end
 
-   private
+  private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
