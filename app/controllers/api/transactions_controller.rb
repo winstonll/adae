740e2370c77d3464d9fa293aed17efa81ced8e23
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-12-04 15:41:43 +0900

    review and transaction API. need to edit index and show method during ios dev.

---
+++ app/controllers/api/transactions_controller.rb
@@ -0,0 +1,54 @@
+module Api
+  class TransactionsController < ApplicationController
+    def index
+      transaction = Transaction.all
+      if item_id = params[:item_id]
+        transaction = Transaction.where(item_id: item_id)
+      end
+      render json: transaction, status: :ok
+    end
+
+    def show
+      transaction = Transaction.find(params[:id])
+      if !transaction.nil?
+        render json: transaction
+      else
+        render json: {
+          error: "No such transaction; check the transaction id",
+          status: 400
+        }, status: 400
+      end
+    end
+
+    def create
+      transaction = Transaction.new(transaction_params)
+
+      if transaction.save
+        render nothing: true, status: 204#, location: user
+      else
+        render json: transaction.errors, status: 422
+      end
+    end
+
+    def destroy
+      transaction = Transaction.find(params[:id])
+
+      if !transaction.nil?
+        Transaction.destroy(params[:id])
+        head :no_content
+      else
+        render json: {
+          error: "No such transaction; check the transaction id",
+          status: 400
+        }, status: 400
+      end
+    end
+
+    private
+
+      def transaction_params
+        params.require(:transactions).permit(:start_date, :end_date,
+        :return_date, :item_id, :buyer_id, :out_scan, :in_scan)
+      end
+  end
+end
