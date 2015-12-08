29ee4bca71d27ac92bf9f4a1d7938eebbbccd46d
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-12-08 15:17:35 +0900

    changed the schema again. Tried to completely remove devise authentication token gem changes. Reinstalled devise on our user model table. Redid our migrations. Added update function to our user controller so users can update their information through API.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -60,6 +60,19 @@ module Api::V1
       end
     end
 
+    def update
+      user = User.find(params[:id])
+      puts user_params
+      puts params[:users][:email]
+
+      if user.update(email: params[:users][:email])
+
+        render json: user, status: 200#, location: [:api, user]
+      else
+        render json: { errors: user.errors }, status: 422
+      end
+    end
+
     private
 
       def user_params
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,28 +4,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  include Authenticable
+
   # rescue_from CanCan::AccessDenied do |exception|
   #   redirect_to root_url, :alert => exception.message
   # end
 
   private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    rescue ActiveRecord::RecordNotFound
-  end
+  #def current_user
+  #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #  rescue ActiveRecord::RecordNotFound
+  #end
 
-  helper_method :current_user
+  #helper_method :current_user
 
-  def ensure_logged_in
-    unless current_user
-      flash[:alert] = "Please log in"
-      session[:previous_url] = request.fullpath
-      redirect_to new_session_path
-    end
-  end
+  #def ensure_logged_in
+  #  unless current_user
+  #    flash[:alert] = "Please log in"
+  #    session[:previous_url] = request.fullpath
+  #    redirect_to new_session_path
+  #  end
+  #end
 
-  def require_user
-    redirect_to root_path unless current_user
-  end
+  #def require_user
+  #  redirect_to root_path unless current_user
+  #end
 end
