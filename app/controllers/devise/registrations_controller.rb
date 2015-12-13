8c3314ef9a37a7fc9355e206c62297982a70f423
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-12-13 20:49:02 +0900

    login, logout, sign up links added to the top of the landing page. sign up link does not work currently

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -4,30 +4,7 @@ class Devise::RegistrationsController < DeviseController
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #include Authenticable
-
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_url, :alert => exception.message
-  # end
-
-  private
-
-  #def current_user
-  #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  #  rescue ActiveRecord::RecordNotFound
-  #end
-
-  #helper_method :current_user
-
-  #def ensure_logged_in
-  #  unless current_user
-  #    flash[:alert] = "Please log in"
-  #    session[:previous_url] = request.fullpath
-  #    redirect_to new_session_path
-  #  end
-  #end
-
-  #def require_user
-  #  redirect_to root_path unless current_user
-  #end
+  def new
+    
+  end
 end
