97aa1c52dad4796ec5383bd133e2701ff3c75e27
Ruben Avila <Ruavilaa@hotmail.com>
2015-12-15 12:28:36 -0500

    changed first name and last name to name and surname in accordance with the schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
  
  	private
  	def user_params
- 	  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
+ 	  params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation,
  	  :phone_number)
  	end
  
