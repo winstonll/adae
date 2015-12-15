ee31db56dde9f45a69252c5f9942b588b6b12711
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-12-16 02:03:49 +0900

    moved all devise views to the proper devise folder in the view folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users
 
   #constraints subdomain: 'api' do
     namespace :api do
