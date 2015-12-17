8be38bc5914993009e514e8d8bafb351e80e6ab8
Atsushi Hirata <atsushi.hirata890@gmail.com>
2015-12-17 14:39:57 +0900

    deleted code we dont need

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -6,20 +6,6 @@ class BaseController < ActionController::Base
 
   include Authenticable
 
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_url, :alert => exception.message
-  # end
-
   private
 
-  #def current_user
-  #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  #  rescue ActiveRecord::RecordNotFound
-  #end
-
-  #helper_method :current_user
-
-  #def require_user
-  #  redirect_to root_path unless current_user
-  #end
 end
