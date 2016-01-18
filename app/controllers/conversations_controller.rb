cd9905ac0083c90010bb7c65bd405ca7232b333e
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-01-18 13:31:06 -0500

    Created the bare bones messages view and the mail inbox view. Edited the quotation marks on the messaging views and back end because they were not closing properly.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,6 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user
-  
+  before_action :authenticate_user!
+
   def index
     @users = User.all
     @conversations = Conversation.all
