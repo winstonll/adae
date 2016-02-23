029d1fa663d84e5701eea8b66b86c0dca6867013
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-02-23 15:30:54 -0500

    updated url of picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,7 +8,7 @@ class Picture < ActiveRecord::Base
 
   has_attached_file :image,
     :path => ":rails_root/public/system/images/:id/:filename",
-    :url  => "/images/:id/:filename",
+    :url  => "/system/images/:id/:filename",
     styles: { small: "400x400", med: "800x800", large: "1200x1200" }
 
   do_not_validate_attachment_file_type :image
