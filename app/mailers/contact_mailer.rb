db32e572f75ceabad1c64e2bac861ff102fcff13
Ruben Avila <Ruavilaa@hotmail.com>
2016-03-07 13:45:46 -0500

    changes to default from and default to in contact model

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
 
-  default :from => "noreply@adae.co"
-  default :to => "ruavilaa@gmail.com"
+  default :from => "mail@adae.co"
+  default :to => "winston@adae.co"
 
   def new_message(contact)
     @contact = contact
-    mail(:subject => "[Adae.tld] #{contact.subject}")
+    mail(:subject => "Adae.co #{contact.subject}")
   end
 
 end
