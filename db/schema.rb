2692cd862f84b2647b8c5622e7dc25a0905623de
tonyideng <tonydeng@Tonys-MacBook-Pro.local>
2016-04-02 18:19:07 -0400

    migrating before deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311164528) do
+ActiveRecord::Schema.define(version: 20160401180344) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -101,12 +101,12 @@ ActiveRecord::Schema.define(version: 20160311164528) do
   end
 
   create_table "requests", force: :cascade do |t|
-    t.string  "title"
-    t.string  "description"
-    t.integer "user_id"
-    t.string  "tags"
-    t.string  "timeframe"
-    t.string  "postal_code"
+    t.string   "title"
+    t.string   "description"
+    t.integer  "user_id"
+    t.string   "tags"
+    t.datetime "timeframe"
+    t.string   "postal_code"
   end
 
   create_table "reviews", force: :cascade do |t|
