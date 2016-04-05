5c685584c611e02d9baed85498566ddfc782a378
Ruben Avila <Ruavilaa@hotmail.com>
2016-04-05 15:11:26 -0400

    only description should be unique for shoutouts

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ActiveRecord::Base
 	validates :title, :description, :user_id, :tags, :timeframe, :postal_code,  presence: true
-	validates :title, :description, :postal_code,  uniqueness: true
+	validates :description,  uniqueness: true
 	
 	belongs_to :user
 end
