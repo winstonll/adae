ae03f0d1d18e5aadcc9c5381271fa1515ae40128
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-04-21 13:02:25 +0900

    testing TCP socket

---
+++ server.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+server = TCPServer.new 1991 # Server bound to port 2000
+
+loop do
+  client = server.accept    # Wait for a client to connect
+  client.puts "Hello !"
+  client.puts "Time is #{Time.now}"
+  client.close
+end
