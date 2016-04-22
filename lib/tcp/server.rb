483b514dc55dd3eb999266dcda5579531c9b5858
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-04-22 17:37:31 +0900

    moved server, client and server control file to lib/directory. Attempting to use eventmachine gem

---
+++ lib/tcp/server.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require 'socket'
+require 'eventmachine'
+
+ module EchoServer
+   def post_init
+     puts "-- someone connected to the echo server!"
+   end
+
+   def receive_data data
+     send_data ">>>you sent: #{data}"
+     close_connection if data =~ /quit/i
+   end
+
+   def unbind
+     puts "-- someone disconnected from the echo server!"
+  end
+end
+
+# Note that this will block current thread.
+EventMachine.run {
+  EventMachine.start_server "1991", 8081, EchoServer
+}
