17472f033c47168571d5d9885e253ecac2ed7e2d
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-04-30 21:15:04 +0900

    Do not have Resque

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Resque.redis = Redis.new(:host => "https://adae.co", :port => 6379)
+  $redis = Redis.new(:host => "https://adae.co", :port => 6379)
 elsif Rails.env == 'development'
   $redis = Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0)
 end
