b545a0f290ccb8d2cca1db5906e01809b7e74282
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-05-03 20:23:08 +0900

    always listen on localhost

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
-if Rails.env == 'production'
-  $redis = Redis.new(:host => "159.203.22.61", :port => 6379, :db => 0)
-elsif Rails.env == 'development'
+#if Rails.env == 'production'
+#  $redis = Redis.new(:host => "159.203.22.61", :port => 6379, :db => 0)
+#elsif Rails.env == 'development'
   $redis = Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0)
-end
+#end
