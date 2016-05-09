5d7776a9814be7bfa05b4cd7969cd43c3169e980
Ruben Avila <Ruavilaa@hotmail.com>
2016-05-09 13:13:57 -0400

    review prompt

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -6,4 +6,11 @@ class SendEmailJob < ActiveJob::Base
     @message = message
     ContactMailer.new_message(@user, @message).deliver_later
   end
+
+  def review(buyer, seller, listing)
+  	@buyer = buyer
+    @seller = seller
+    @listing = listing
+    ContactMailer.review_prompt(@buyer, @seller, @listing).deliver_later
+  end
 end
