230e890cf4439c08e9b59c0e56722268b3d85bf2
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-05-19 16:31:58 +0900

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160518144540) do
 
   create_table "conversations", force: :cascade do |t|
