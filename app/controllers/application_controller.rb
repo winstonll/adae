93c1fd2f2d2e7a6c8a70a1c6f5cf5e90b62fa4b8
Ruben Avila <Ruavilaa@hotmail.com>
2016-06-10 14:41:54 -0400

    sign in path and sign out path changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    items_path
+    root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-   items_path
+   root_path
   end
 
   private
