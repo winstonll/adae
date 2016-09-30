adb99f371ced6e400d9a1360926e0ab6ee3db1ea
Ruben Avila <Ruavilaa@hotmail.com>
2016-09-30 14:26:12 -0400

    ensuring correct path in controller allow signup in navbar and bring back item show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    root_path
+    items_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-   root_path
+    items_path
   end
 
   private
